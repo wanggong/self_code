09bc8c3508d120f733cd2e3e394c9d5506e023cc
wgz <wgz@ubuntu.ubuntu-domain>
2013-09-28 22:55:49 +0800

    move take pic script to window dir

--- script/auto_take_pic.bat
+++
@@ -1,5 +0,0 @@
-adb shell input keyevent 27
-sleep 2
-adb shell mv /sdcard/DCIM/Camera/*.jpg /sdcard/DCIM/Camera/test.jpg
-adb pull /sdcard/DCIM/Camera/test.jpg takepic/%1.jpg
-adb shell rm /sdcard/DCIM/Camera/*
