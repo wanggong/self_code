2a6bc5010b6f134236e4167714351d290a9f9949
unknown <wanggongzhen@LEADCORETECH.COM>
2017-07-10 14:10:02 +0800

    remvoe some useless debugfs folder

--- debugfs/system/core/android_backtrace/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := android_backtrace.c
-LOCAL_MODULE := libandroid_backtrace
-LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libcorkscrew liblog
-include $(BUILD_SHARED_LIBRARY)
